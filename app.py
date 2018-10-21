f4531eb78376e851dba7f338f3cda09a429d871f
Octavio Fernandes <octaviobffernandes@gmail.com>
2018-10-21 15:02:01 +0200

    Add automatic package installation; loading env variables on startup

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import os
 from instance.init import create_app
+from dotenv import load_dotenv
+from pathlib import Path
 
+env_path = Path('.') / 'vars.env'
+load_dotenv(dotenv_path=env_path)
 
 config_name = os.getenv('APP_SETTINGS')
 app = create_app(config_name)
