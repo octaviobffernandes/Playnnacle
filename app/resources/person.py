ed1085e6a8ba0cb3dad9bfc8b1d9fa52c4cc6d75
Heitor Fernandes <hbffernandes@email.arizona.edu>
2018-11-27 22:43:44 -0700

    Parse post requests with webargs
    Changes replace using request.get_json(), by now using webargs's parser. This permits defining arguments as a mapping rather than pulling values off of request objects.
    Changes are a simple test to see it working, before implementing webargs to game resources.
    
    Note: webargs is integrated with marhsmallow and facilitates using it.

--- app/resources/person.py
+++ app/resources/person.py
@@ -4,22 +4,33 @@ from app.models import Person
 from app.models import Pet
 from mongoengine import *
 import datetime
+from webargs import fields
+from webargs.flaskparser import parser
+import sys
 
 class PersonResource(Resource):
     def get(self):
         return Response(Person.objects.to_json(), mimetype='application/json')
 
     def post(self):
+        personal_info = {
+            "FirstName": fields.Str(required=True),
+            "LastName": fields.Str(required=True),
+            "Bsn": fields.Int(required=True),
+            "Pets": fields.List(fields.Dict, missing=[]),
+        }
+
+        args = parser.parse(personal_info, request)
+
         person = Person()
-        req = request.get_json()
         
-        person.first_name=req['FirstName']
-        person.last_name=req['LastName']
-        person.bsn=req['Bsn']
+        person.first_name=args['FirstName']
+        person.last_name=args['LastName']
+        person.bsn=args['Bsn']
         person.birth_date = datetime.date(1985, 10, 2)
         
         person.pets = []
-        for p in req['Pets']:
+        for p in args['Pets']:
             person.pets.append(Pet(name=p['Name'], breed=p['Breed']))
         person.save()
         return 'ok'
