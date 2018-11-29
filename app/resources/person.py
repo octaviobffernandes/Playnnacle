48600400ae73e5f92ec26812239ab2e330ab661c
Heitor Fernandes <hbffernandes@email.arizona.edu>
2018-11-28 20:59:30 -0700

    Make factory class to create Person object from post request
    Abstracts the creation of a Person object from the resource package (i.e., the PersonResource class), by creating a new class that functions as the factory of Person objects. The factory class uses marhsmallow's deserializiation to map the request json directly into a newly created Person object. This permits avoiding creation of dictionary when taking a post request, to map it to the new Person object. Thus it allows regaining control, compared to the previous design of the program.
    
    Acknowledgements: Marcelo Inhaquites helped all the way.

--- app/resources/person.py
+++ app/resources/person.py
@@ -1,11 +1,8 @@
 from flask_restful import Resource, request
 from flask import Response
 from app.models import Person
-from app.models import Pet
+from app.helpers import PersonInformation
 from mongoengine import *
-import datetime
-from webargs import fields
-from webargs.flaskparser import parser
 import sys
 
 class PersonResource(Resource):
@@ -13,24 +10,9 @@ class PersonResource(Resource):
         return Response(Person.objects.to_json(), mimetype='application/json')
 
     def post(self):
-        personal_info = {
-            "FirstName": fields.Str(required=True),
-            "LastName": fields.Str(required=True),
-            "Bsn": fields.Int(required=True),
-            "Pets": fields.List(fields.Dict, missing=[]),
-        }
+        person_information = PersonInformation()
 
-        args = parser.parse(personal_info, request)
-
-        person = Person()
-        
-        person.first_name=args['FirstName']
-        person.last_name=args['LastName']
-        person.bsn=args['Bsn']
-        person.birth_date = datetime.date(1985, 10, 2)
-        
-        person.pets = []
-        for p in args['Pets']:
-            person.pets.append(Pet(name=p['Name'], breed=p['Breed']))
+        person = person_information.load(request.get_json())
         person.save()
+
         return 'ok'
