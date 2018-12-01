0f660b8a81fa2401ba233d276b8a7eebd60335bd
Heitor Fernandes <hbffernandes@email.arizona.edu>
2018-11-30 23:22:28 -0700

    Add person builder with validation, between resource and model
    This implements a builder class (whose name is up for discussion) for Person. This class does validation for request fields and specifies their respective attribute names. Code for actually assigning values to Person attributes remains the same (i.e., very concise).
    
    Note that a class for validating Pet-related fields is also implemented.
    
    It remains to be discussed (1) how Pet object will be created when assigning values to Person attributes; (2) whether splitting the PersonBuilder class into a validator and a person maker would be ideal.

--- app/models/person.py
+++ app/models/person.py
@@ -1,6 +1,5 @@
 from mongoengine import *
 from .pet import Pet
-import json
 
 
 class Person(Document):
@@ -9,10 +8,3 @@ class Person(Document):
     bsn = IntField()
     birth_date = DateTimeField()
     pets = ListField(EmbeddedDocumentField(Pet))
-
-    @staticmethod
-    def from_json(json):
-        person = Person()
-        for k, v in json.items():
-            setattr(person, k, v)
-        return person
--- app/resources/person.py
+++ app/resources/person.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource, request
 from flask import Response
 from app.models import Person
+from app.builders import PersonBuilder
 from mongoengine import *
 
 
@@ -9,12 +10,13 @@ class PersonResource(Resource):
         return Response(Person.objects.to_json(), mimetype='application/json')
 
     def post(self):
-        person = Person.from_json(request.get_json())
+        person_information = PersonBuilder()
+        person = person_information.load(request.get_json())
 
         print(person.first_name)
         print(person.last_name)
         print(person.bsn)
         print(person.pets)
-        # person.save()
+        #person.save()
 
         return 'ok'
