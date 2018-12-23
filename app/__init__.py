3c94cc2ad0a0d8e9e7b8fecb6879252d6c682c6a
Octavio Fernandes <octaviobffernandes@gmail.com>
2018-12-23 17:59:35 +0100

    Add swagger to application
    Introduce documentation initially only to person resource

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from flask_restful import Api
 from .instance.config import app_config
 from .resources import Game, Games, PersonResource, CountryResource
 from mongoengine import connect
-
+from flask_restful_swagger import swagger
 
 vars_path = Path('.') / 'vars.env'
 private_vars_path = Path('.') / 'private_vars.env'
@@ -16,7 +16,7 @@ load_dotenv(dotenv_path=private_vars_path)
 config_name = os.getenv('APP_SETTINGS')
 application = Flask(__name__)
 application.config.from_object(app_config[config_name])
-api = Api(application)
+api = swagger.docs(Api(application), apiVersion='0.1')
 api.add_resource(Game, '/games/<string:name>')
 api.add_resource(Games, '/games')
 # api.add_resource(ImportList, '/importsummary')
--- app/resources/personresource.py
+++ app/resources/personresource.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource, request
 from flask import Response
+from flask_restful_swagger import swagger
 from app.schemas import PersonSchema, GetPeopleSchema
 from app.services import PersonService
 import json
@@ -7,6 +8,63 @@ import json
 class PersonResource(Resource):
     service = PersonService()
 
+    @swagger.operation(
+        notes='some really good notes',
+        responseClass=PersonSchema.__name__,
+        nickname='GetAllPeople',
+        parameters=[
+            {
+              "name": "bsn",
+              "description": "Exact BSN number of the person",
+              "required": False,
+              "allowMultiple": False,
+              "dataType": "string",
+              "paramType": "query"
+            },
+            {
+              "name": "firstName",
+              "description": "Part of the first name of the person",
+              "required": False,
+              "allowMultiple": False,
+              "dataType": "string",
+              "paramType": "query"
+            },
+            {
+              "name": "lastName",
+              "description": "Part of the last name of the person",
+              "required": False,
+              "allowMultiple": False,
+              "dataType": "string",
+              "paramType": "query"
+            },
+            {
+              "name": "limit",
+              "description": "Limit of items per page. Default is 5",
+              "required": False,
+              "allowMultiple": False,
+              "dataType": "int",
+              "paramType": "query"
+            },
+            {
+              "name": "offset",
+              "description": "Number of items to skip in the result set. Default is 0",
+              "required": False,
+              "allowMultiple": False,
+              "dataType": "int",
+              "paramType": "query"
+            }                        
+          ],
+        responseMessages=[
+            {
+              "code": 200,
+              "message": "Success"
+            },
+            {
+              "code": 500,
+              "message": "Internal Server Error"
+            }
+          ]
+        )    
     def get(self):
         req = GetPeopleSchema().load(request.args).data
         people = self.service.get_many(req)
@@ -14,6 +72,35 @@ class PersonResource(Resource):
         result = [PersonSchema().dump(p) for p in people]
         return result, 200
 
+    @swagger.operation(
+        notes='some really good notes',
+        responseClass=PersonSchema.__name__,
+        nickname='SavePerson',
+        parameters=[
+            {
+              "name": "person",
+              "description": "Information about the person to be saved",
+              "required": True,
+              "allowMultiple": False,
+              "dataType": PersonSchema.__name__,
+              "paramType": "body"
+            }                        
+          ],
+        responseMessages=[
+            {
+              "code": 200,
+              "message": "Success"
+            },
+            {
+              "code": 400,
+              "message": "Bad Request"
+            },
+            {
+              "code": 500,
+              "message": "Internal Server Error"
+            }
+          ]
+        )   
     def post(self):
         person_schema = PersonSchema()
         person_model = person_schema.load(request.get_json())
