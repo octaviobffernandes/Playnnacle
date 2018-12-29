2f76d7d08e7f27e7e2497044ed0a8a02f7d5dd33
Heitor Fernandes <hbffernandes@email.arizona.edu>
2018-12-29 19:58:48 -0200

    Create url_prefix in blueprints, simplify endpoints
    Refactor blueprints to make /people and /country be prefixes. This is good practice to facilitate future work and make it clear (for example, when new endpoints are created, such as /people/id). /people is not part of each new endpoint, it is simply a static prefix.

--- app/country_module/blueprint.py
+++ app/country_module/blueprint.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from . import CountryResource
 
-country_blueprint = Blueprint('country', __name__)
+country_blueprint = Blueprint('country', __name__, url_prefix='/country')
 country_view = CountryResource.as_view('countries')
 
-country_blueprint.add_url_rule('/country/', view_func=country_view)
+country_blueprint.add_url_rule('/', view_func=country_view)
--- app/person_module/blueprint.py
+++ app/person_module/blueprint.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from . import PeopleResource
 
-person_blueprint = Blueprint('person', __name__)
+person_blueprint = Blueprint('person', __name__, url_prefix='/people')
 person_view = PeopleResource.as_view('people')
 
-person_blueprint.add_url_rule('/people/', view_func=person_view)
+person_blueprint.add_url_rule('/', view_func=person_view)
